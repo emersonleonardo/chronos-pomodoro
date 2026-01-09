366e043d9d43d821c5b43b391830f2a2dd018447
Leonardo Lima <leonardodelima96@hotmail.com.br>
2026-01-09 00:57:24 -0300

    Adicionando mudança de tema da página

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react'
 import styles from './styles.module.css'
 
 import {
   HourglassIcon,
   HouseIcon,
+  MoonIcon,
   SettingsIcon,
   SunIcon,
   TimerIcon,
 } from 'lucide-react'
 
+type AvailableThemes = 'dark' | 'light'
+
 export function Header() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const localStorageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark'
+
+    return localStorageTheme
+  })
+
+  const themeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  }
+
+  function handleThemeChange() {
+    setTheme(prev => (prev === 'dark' ? 'light' : 'dark'))
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
   return (
     <div className={styles.header}>
       <div className={styles.icon}>
@@ -17,23 +42,35 @@ export function Header() {
       <h1 className={styles.title}>Pomodoro</h1>
       <nav className={styles.listMenu}>
         <li>
-          <button>
-            <TimerIcon />
-          </button>
-        </li>
-        <li>
-          <button>
+          <a
+            href='#'
+            aria-label='Ir para a página inicial'
+            title='Página inicial'
+          >
             <HouseIcon />
-          </button>
+          </a>
         </li>
         <li>
-          <button>
+          <a href='#' aria-label='Ir para o histórico' title='Histórico'>
+            <TimerIcon />
+          </a>
+        </li>
+        <li>
+          <a
+            href='#'
+            aria-label='Ir para as configurações'
+            title='Configurações'
+          >
             <SettingsIcon />
-          </button>
+          </a>
         </li>
         <li>
-          <button>
-            <SunIcon />
+          <button
+            aria-label='Alterar o tema da página'
+            title='Alterar o tema da página'
+            onClick={handleThemeChange}
+          >
+            {themeIcon[theme]}
           </button>
         </li>
       </nav>
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 export default function Timer() {
   return (
-    <div>
+    <div className={styles.timerContainer}>
       <p className={styles.timer}>00:00</p>
     </div>
   )
